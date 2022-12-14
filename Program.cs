b97054c111311341c7fc4c62b63053a7a652202c
Roberta Tagliari Hoffmann <45298958+robertath@users.noreply.github.com>
2022-12-14 13:27:54 +0000

    [feature2]update:program.cs2

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Editing the same line (file from feature-2 branch)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
